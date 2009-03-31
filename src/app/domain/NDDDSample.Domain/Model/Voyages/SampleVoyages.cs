a69b26e62c1c074186b9d4007c335b7ad4004294
artur.trosin <artur.trosin@8496d0c4-1a5a-11de-9308-ab3afd9c0aba>
2009-03-31 11:20:26 +0000

    1. Refacotor java Get methods to Props 2. Code arrangement + fixed Inconsistent line ending style
    git-svn-id: http://ndddsample.googlecode.com/svn/trunk@16 8496d0c4-1a5a-11de-9308-ab3afd9c0aba

--- src/app/domain/NDDDSample.Domain/Model/Voyages/SampleVoyages.cs
+++ src/app/domain/NDDDSample.Domain/Model/Voyages/SampleVoyages.cs
@@ -14,7 +14,11 @@
     /// </summary>
     public class SampleVoyages
     {
-        public static readonly IDictionary<VoyageNumber, Voyage> ALL = new Dictionary<VoyageNumber, Voyage>();
+        private static readonly IDictionary<VoyageNumber, Voyage> ALL = new Dictionary<VoyageNumber, Voyage>();
+
+        #region Depricated
+
+        // TODO CM00[1-6] and createVoyage are deprecated. Remove and refactor tests.
         public static readonly Voyage CM001 = CreateVoyage("CM001", SampleLocations.STOCKHOLM, SampleLocations.HAMBURG);
         public static readonly Voyage CM002 = CreateVoyage("CM002", SampleLocations.HAMBURG, SampleLocations.HONGKONG);
         public static readonly Voyage CM003 = CreateVoyage("CM003", SampleLocations.HONGKONG, SampleLocations.NEWYORK);
@@ -22,6 +26,14 @@
         public static readonly Voyage CM005 = CreateVoyage("CM005", SampleLocations.CHICAGO, SampleLocations.HAMBURG);
         public static readonly Voyage CM006 = CreateVoyage("CM006", SampleLocations.HAMBURG, SampleLocations.HANGZOU);
 
+        #endregion
+
+        #region Named Voyages
+
+        /// <summary>
+        /// Voyage number 0300A (by airplane)   
+        /// Dallas - Hamburg - Stockholm - Helsinki   
+        /// </summary>
         public static readonly Voyage DALLAS_TO_HELSINKI =
             new Voyage.Builder(new VoyageNumber("0300A"), SampleLocations.DALLAS).
                 AddMovement(SampleLocations.HAMBURG, DateTime.Parse("2008-10-29 03:30"),
@@ -32,12 +44,20 @@
                             DateTime.Parse("2008-11-02 11:15")).
                 Build();
 
+        /// <summary>
+        /// Voyage number 0301S (by ship)
+        /// Dallas - Hamburg - Stockholm - Helsinki, alternate route
+        /// </summary>
         public static readonly Voyage DALLAS_TO_HELSINKI_ALT =
             new Voyage.Builder(new VoyageNumber("0301S"), SampleLocations.DALLAS).
                 AddMovement(SampleLocations.HELSINKI, DateTime.Parse("2008-10-29 03:30"),
                             DateTime.Parse("2008-11-05 15:45")).
                 Build();
 
+        /// <summary>
+        ///  Voyage number 0400S (by ship)
+        /// Helsinki - Rotterdam - Shanghai - Hongkong
+        /// </summary>
         public static readonly Voyage HELSINKI_TO_HONGKONG =
             new Voyage.Builder(new VoyageNumber("0400S"), SampleLocations.HELSINKI).
                 AddMovement(SampleLocations.ROTTERDAM, DateTime.Parse("2008-11-04 05:50"),
@@ -48,6 +68,10 @@
                             DateTime.Parse("2008-11-28 13:37")).
                 Build();
 
+        /// <summary>
+        /// Voyage number 0200T (by train)
+        /// New York - Chicago - Dallas
+        /// </summary>
         public static readonly Voyage NEW_YORK_TO_DALLAS =
             new Voyage.Builder(new VoyageNumber("0200T"), SampleLocations.NEWYORK).
                 AddMovement(SampleLocations.CHICAGO, DateTime.Parse("2008-10-24 07:00"),
@@ -56,8 +80,25 @@
                             DateTime.Parse("2008-10-25 19:30")).
                 Build();
 
+        /// <summary>
+        ///Voyage number 0100S (by ship)
+        ///Hongkong - Hangzou - Tokyo - Melbourne - New York   
+        ///</summary>      
+        public static Voyage HONGKONG_TO_NEW_YORK =
+            new Voyage.Builder(new VoyageNumber("0100S"), SampleLocations.HONGKONG).
+                AddMovement(SampleLocations.HANGZOU, DateTime.Parse("2008-10-01 12:00"),
+                            DateTime.Parse("2008-10-03 14:30")).
+                AddMovement(SampleLocations.TOKYO, DateTime.Parse("2008-10-03 21:00"),
+                            DateTime.Parse("2008-10-06 06:15")).
+                AddMovement(SampleLocations.MELBOURNE, DateTime.Parse("2008-10-06 11:00"),
+                            DateTime.Parse("2008-10-12 11:30")).
+                AddMovement(SampleLocations.NEWYORK, DateTime.Parse("2008-10-14 12:00"),
+                            DateTime.Parse("2008-10-23 23:10")).
+                Build();
 
-        // TODO CM00[1-6] and createVoyage are deprecated. Remove and refactor tests.
+        #endregion
+
+        #region v[1-4] Voyages
 
         public static readonly Voyage v100 = new Voyage.Builder(new VoyageNumber("V100"), SampleLocations.HONGKONG).
             AddMovement(SampleLocations.TOKYO, DateTime.Parse("2009-03-03"), DateTime.Parse("2009-03-05")).
@@ -84,19 +125,9 @@
             Build();
 
 
-        //Voyage number 0100S (by ship)
-        //Hongkong - Hangzou - Tokyo - Melbourne - New York         
-        public static Voyage HONGKONG_TO_NEW_YORK =
-            new Voyage.Builder(new VoyageNumber("0100S"), SampleLocations.HONGKONG).
-                AddMovement(SampleLocations.HANGZOU, DateTime.Parse("2008-10-01 12:00"),
-                            DateTime.Parse("2008-10-03 14:30")).
-                AddMovement(SampleLocations.TOKYO, DateTime.Parse("2008-10-03 21:00"),
-                            DateTime.Parse("2008-10-06 06:15")).
-                AddMovement(SampleLocations.MELBOURNE, DateTime.Parse("2008-10-06 11:00"),
-                            DateTime.Parse("2008-10-12 11:30")).
-                AddMovement(SampleLocations.NEWYORK, DateTime.Parse("2008-10-14 12:00"),
-                            DateTime.Parse("2008-10-23 23:10")).
-                Build();
+        #endregion       
+
+        #region Static Methods
 
         private static Voyage CreateVoyage(string id, Location from, Location to)
         {
@@ -116,18 +147,22 @@
         {
             return ALL[voyageNumber];
         }
-        
+
+        #endregion
+
+        #region Static constr
+
         static SampleVoyages()
         {
             //TODO: atrosin verify if that logic works correctly
-            foreach (var fieldInfo in typeof(SampleVoyages).GetFields(BindingFlags.Static))
+            foreach (var fieldInfo in typeof (SampleVoyages).GetFields(BindingFlags.Static))
             {
                 try
                 {
-                    if (fieldInfo.FieldType == typeof(Voyage))
+                    if (fieldInfo.FieldType == typeof (Voyage))
                     {
-                        var voyage = (Voyage)fieldInfo.GetValue(null);
-                        ALL.Add(voyage.VoyageNumber(), voyage);
+                        var voyage = (Voyage) fieldInfo.GetValue(null);
+                        ALL.Add(voyage.VoyageNumber, voyage);
                     }
                 }
                 catch (Exception e)
@@ -135,7 +170,8 @@
                     throw new Exception("Can't initialize Sample Locations", e);
                 }
             }
-
         }
+
+        #endregion
     }
 }
