371c8bb19617a2870fc90251fe44f25a261ffca3
gorgan.evghenii <gorgan.evghenii@8496d0c4-1a5a-11de-9308-ab3afd9c0aba>
2009-04-23 09:00:14 +0000

    CarrierMovementTest
    git-svn-id: http://ndddsample.googlecode.com/svn/trunk@53 8496d0c4-1a5a-11de-9308-ab3afd9c0aba

---
+++ src/test/NDDDSample.Tests/Domain/Model/Voyages/CarrierMovementTest.cs
@@ -0,0 +1,44 @@
+namespace NDDDSample.Tests.Domain.Model.Voyages
+{
+    #region Usings
+
+    using System;
+    using NDDDSample.Domain.Model.Locations;
+    using NDDDSample.Domain.Model.Voyages;
+    using NUnit.Framework;
+
+    #endregion
+
+    [TestFixture]
+    public class CarrierMovementTest
+    {
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException), UserMessage = "Should not accept null constructor arguments")]
+        public void testConstructor()
+        {
+            new CarrierMovement(null, null, new DateTime(), new DateTime());
+            new CarrierMovement(SampleLocations.STOCKHOLM, null, new DateTime(), new DateTime());
+        }
+
+        [Test]
+        public void testSameValueAsEqualsHashCode()
+        {
+            var cm1 = new CarrierMovement(SampleLocations.STOCKHOLM, SampleLocations.HAMBURG, new DateTime(), new DateTime());
+            var cm2 = new CarrierMovement(SampleLocations.STOCKHOLM, SampleLocations.HAMBURG, new DateTime(), new DateTime());
+            var cm3 = new CarrierMovement(SampleLocations.HAMBURG, SampleLocations.STOCKHOLM, new DateTime(), new DateTime());
+            var cm4 = new CarrierMovement(SampleLocations.HAMBURG, SampleLocations.STOCKHOLM, new DateTime(), new DateTime());            
+
+            Assert.IsTrue(cm1.SameValueAs(cm2));
+            Assert.IsFalse(cm2.SameValueAs(cm3));
+            Assert.IsTrue(cm3.SameValueAs(cm4));
+
+            Assert.IsTrue(cm1.Equals(cm2));
+            Assert.IsFalse(cm2.Equals(cm3));
+            Assert.IsTrue(cm3.Equals(cm4));
+
+            Assert.IsTrue(cm1.GetHashCode() == cm2.GetHashCode());
+            Assert.IsFalse(cm2.GetHashCode() == cm3.GetHashCode());
+            Assert.IsTrue(cm3.GetHashCode() == cm4.GetHashCode());
+        }
+    }
+}
