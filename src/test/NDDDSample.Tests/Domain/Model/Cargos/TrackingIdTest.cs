86ed7a8110d9705ef8300800273f7f1ea3eed4d6
artur.trosin <artur.trosin@8496d0c4-1a5a-11de-9308-ab3afd9c0aba>
2009-04-23 07:58:58 +0000

    Creation of Domain Model Tests folder structure
    git-svn-id: http://ndddsample.googlecode.com/svn/trunk@51 8496d0c4-1a5a-11de-9308-ab3afd9c0aba

---
+++ src/test/NDDDSample.Tests/Domain/Model/Cargos/TrackingIdTest.cs
@@ -0,0 +1,22 @@
+﻿namespace NDDDSample.Tests.Domain.Model.Cargos
+{
+    #region Usings
+
+    using System;
+    using NDDDSample.Domain.Model.Cargos;
+    using NUnit.Framework;
+
+    #endregion
+
+    [TestFixture]
+    public class TrackingIdTest
+    {
+        [ExpectedException(typeof (ArgumentNullException), UserMessage = "Should't accept null constructor arguments")
+        ]
+        [Test]
+        public void Constructor()
+        {
+            new TrackingId(null);
+        }
+    }
+}
