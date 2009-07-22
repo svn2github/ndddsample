ccadf42aa388eac11ad1d991edfe6495c6709b07
artur.trosin <artur.trosin@8496d0c4-1a5a-11de-9308-ab3afd9c0aba>
2009-07-22 09:36:50 +0000

    Project BookingRemoteService Refactoring
    git-svn-id: http://ndddsample.googlecode.com/svn/trunk@77 8496d0c4-1a5a-11de-9308-ab3afd9c0aba

---
+++ src/app/interfaces/NDDDSample.Interfaces.BookingRemoteService.Common/Dto/CargoRoutingDTO.cs
@@ -0,0 +1,83 @@
+﻿namespace NDDDSample.Interfaces.BookingRemoteService.Common.Dto
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Runtime.Serialization;
+    using Infrastructure.Utils;
+
+    /// <summary>
+    /// DTO for registering and routing a cargo.
+    /// </summary>
+    [Serializable]
+    [DataContract]
+    public class CargoRoutingDTO
+    {
+        private readonly DateTime arrivalDeadline;
+        private readonly string finalDestination;
+        private readonly IList<LegDTO> legs;
+        private readonly bool misrouted;
+        private readonly string origin;
+        private readonly string trackingId;
+
+
+        /// <summary>
+        /// Constructor.   
+        /// </summary>
+        /// <param name="trackingId">trackingId</param>
+        /// <param name="origin">origin</param>
+        /// <param name="finalDestination">finalDestination</param>
+        /// <param name="arrivalDeadline">arrivalDeadline</param>
+        /// <param name="misrouted">misrouted</param>
+        public CargoRoutingDTO(string trackingId, string origin, string finalDestination, DateTime arrivalDeadline,
+                               bool misrouted)
+        {
+            this.trackingId = trackingId;
+            this.origin = origin;
+            this.finalDestination = finalDestination;
+            this.arrivalDeadline = arrivalDeadline;
+            this.misrouted = misrouted;
+            legs = new List<LegDTO>();
+        }
+
+        public string TrackingId
+        {
+            get { return trackingId; }
+        }
+
+        public string Origin
+        {
+            get { return origin; }
+        }
+
+        public string FinalDestination
+        {
+            get { return finalDestination; }
+        }
+
+        public void AddLeg(string voyageNumber, string from, string to, DateTime loadTime, DateTime unloadTime)
+        {
+            legs.Add(new LegDTO(voyageNumber, from, to, loadTime, unloadTime));
+        }
+
+
+        public IList<LegDTO> Legs
+        {
+            get { return new List<LegDTO>(legs).AsReadOnly(); }
+        }
+
+        public bool IsMisrouted
+        {
+            get { return misrouted; }
+        }
+
+        public bool IsRouted
+        {
+            get { return !legs.IsEmpty(); }
+        }
+
+        public DateTime ArrivalDeadline
+        {
+            get { return arrivalDeadline; }
+        }
+    }
+}
---
+++ src/app/interfaces/NDDDSample.Interfaces.BookingRemoteService.Common/Dto/LocationDTO.cs
@@ -0,0 +1,37 @@
+﻿namespace NDDDSample.Interfaces.BookingRemoteService.Common.Dto
+{
+    using System;
+    using System.Runtime.Serialization;
+
+    /// <summary>
+    /// Location DTO.
+    /// </summary>
+    [Serializable]
+    [DataContract]
+    public class LocationDTO
+    {
+        private readonly string name;
+        private readonly string unLocode;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="unLocode">UN Location Code</param>
+        /// <param name="name">Location Name</param>
+        public LocationDTO(string unLocode, string name)
+        {
+            this.unLocode = unLocode;
+            this.name = name;
+        }
+
+        public string UnLocode
+        {
+            get { return unLocode; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+        }
+    }
+}
